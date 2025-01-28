63dd5fd64665fddbdb61649c68a1a48195666b7e
AnlixyStudy <alex19mu9kg26@gmail.com>
2025-01-28 15:59:55 +0300

    update 28.01.25

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 def hello_world(request): 
     return HttpResponse('привет мир')
 
 def home(request):
-    return render(request, 'home.html')
+    todos = Todo.objects.all()
+    return render(request, 'home.html', {'todos': todos})
 
 
 # Create your views here.
